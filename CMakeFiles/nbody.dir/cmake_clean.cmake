00d809c4cae79ef3be6d2fd58ec93d9504c42463
Armaan Sachdeva <141866785+BBCnewslondon@users.noreply.github.com>
2025-11-26 21:25:46 +0000

    added a new animation

---
+++ CMakeFiles/nbody.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nbody.dir/main_nbody.cpp.o"
+  "CMakeFiles/nbody.dir/main_nbody.cpp.o.d"
+  "nbody"
+  "nbody.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nbody.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
